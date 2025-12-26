6d7b4be6dd8d7abef113f3ce20f5cf3f41f94d38
dmitry <149298260+dimaaa0@users.noreply.github.com>
2025-12-26 15:22:05 +0500

    Refactor MusicList component to display music details

--- app/components/MusicList.tsx
+++ app/components/MusicList.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
 
 const MusicList = () => {
-  return (
-    <div>MusicList</div>
-  )
+    return (
+        <div className="card flex justify-between w-full p-4 bg-amber-200">
+
+            <div className="title flex gap-6">
+                <div className="bg-black w-16 h-full"></div>
+                <div className="name flex flex-col justify-center">
+                    <h1>Midnight Dreams</h1>
+                    <h3>Aurora Waves</h3>
+                </div>
+            </div>
+
+
+            <div className="details flex flex-col items-start">
+                <h3>4:30</h3>
+                <button>Add to favorites</button>
+                <button>settings</button>
+            </div>
+
+
+        </div>
+    )
 }
 
 export default MusicList
