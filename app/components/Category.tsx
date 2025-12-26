33d10073f0a7e8c355334f9c591f65e5a1725890
dmitry <149298260+dimaaa0@users.noreply.github.com>
2025-12-26 15:21:42 +0500

    Refactor Category component to manage categories state

--- app/components/Category.tsx
+++ app/components/Category.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+'use client'
 
-const Category = () => {
-  return (
-    <div>Category</div>
-  )
+import { useState } from "react";
+
+interface CategoryType {
+    id: number;
+    label: string;
+    active: boolean;
 }
 
-export default Category
+export default function Home({ }) {
+    const [categories, setCategories] = useState<CategoryType[]>([
+        { id: 1, label: 'Favorites', active: false },
+        { id: 2, label: 'All', active: true },
+        { id: 3, label: 'Jazz', active: false },
+        { id: 4, label: 'Pop', active: false },
+        { id: 5, label: 'Rock', active: false },
+        { id: 6, label: 'Hip-hop', active: false },
+        { id: 7, label: 'Electronic', active: false }
+    ]);
+
+    const handleClick = (id: number) => {
+        setCategories((prev) =>
+            prev.map((cat) =>
+                cat.id === id ? { ...cat, active: true } : { ...cat, active: false }
+            )
+        );
+    };
+
+    return (
+        <div className="flex justify-start gap-2 p-4" >
+            {categories.map((cat) => (
+                <button
+                    key={cat.id}
+                    onClick={() => handleClick(cat.id)}
+                    className={`px-4 py-2 rounded-lg cursor-pointer transition-colors ${cat.active
+                        ? "bg-blue-600 text-white"
+                        : "bg-[#121926be] text-gray-300 hover:bg-[#1c2638]"
+                        }`}
+                >
+                    {cat.label}
+                </button>
+            ))}
+        </div>
+    );
+}
